0614bcf4136463c440e9d7ae87c7594009217959
meriamBenaliFellague <125749607+meriamBenaliFellague@users.noreply.github.com>
2023-05-12 00:29:11 +0100

    Add files via upload

---
+++ DoublyLinkedList.c
@@ -0,0 +1,112 @@
+#include<stdio.h>
+#include<stdlib.h>
+typedef struct node{
+	int data;
+	struct node *next;
+	struct node *prvs;
+}Node;
+Node *head=NULL,*p,*q;
+
+int ReadNumberOfNode();
+int ReadInput();
+void AddNode(int x);
+void display(Node *head);
+int ReadDeletNode();
+int length(Node *head);
+Node *DeletNode(int z,Node *head,int b);
+
+int main(){int number,nbrNode,value;
+nbrNode=ReadNumberOfNode();
+for(int i=0;i<nbrNode;i++){
+	number=ReadInput();
+	AddNode(number);
+}
+display(head);
+
+value=ReadDeletNode();
+display(DeletNode(value,head,nbrNode));
+
+}
+
+int ReadNumberOfNode()
+{int b;
+  printf("enter the number of node: ");
+  scanf("%d",&b);
+  return b;
+}
+
+int ReadInput()
+{int y;
+  printf("enter number: ");
+  scanf("%d",&y);
+  return y;
+}
+
+void AddNode(int x){
+  p=(Node*)malloc(sizeof(Node));
+  p->data=x;
+  if(head==NULL){
+  	head=p;
+  	p->next=NULL;
+  	p->prvs=NULL;
+  }else{
+  	p->next=head;
+  	head->prvs=p;
+  	head=p;
+  	p->prvs=NULL;
+  }
+}
+
+void display(Node *head){
+	p=head;
+	while(p!=NULL){
+		printf("%d\n",p->data);
+		p=p->next;
+	}
+}
+
+int ReadDeletNode()
+{int a;
+  printf("Enter the node to be deleted: ");
+  scanf("%d",&a);
+  return a;
+}
+
+int length(Node *head){
+	if(head==NULL){
+		printf("list is empty");
+		return -1;
+	}else{
+		p=head;int c=0;
+		do{
+			p=p->next;
+			c++;
+		}while(p!=head);
+		return c;
+	}
+}
+
+Node *DeletNode(int z,Node *head,int b){
+  p=head;int count=0;
+  while(p->data!=z){
+  	count++;
+  	p=p->next;
+  }
+  if(count==0){
+  	q=head;
+  	head=q->next;
+  	free(q);
+  }else{
+  	if(count==b-1){
+  		q=p->prvs;
+  		q->next=NULL;
+		free(p);
+	}else{
+		q=p->prvs;
+  		q->next=p->next;
+  		p->next->prvs=q;
+		free(p);
+	}
+  }
+  return head;
+}
